69d27362dfbbb316adb77d64ffe82475ddc9a043
James Acklin <acklin@Jamess-MacBook-Air.local>
2016-04-26 16:17:25 -0400

    Changes to puppy.py

--- puppy.py
+++ puppy.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python
+import datetime
+
+# Define event class
+class Event(object):
+    def __init__(self):
+        self.time = datetime.datetime.utcnow()
+
+# Instantiate new event
+newEvent = Event()
+print newEvent.time

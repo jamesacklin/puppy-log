bea395457dd403be93d8f3645972afa202a82fda
James Acklin <acklin@Jamess-MacBook-Air.local>
2016-04-26 16:22:47 -0400

    Adds reading material

--- puppy.py
+++ puppy.py
@@ -2,10 +2,16 @@
 import datetime
 
 # Define event class
-class Event(object):
+class DogEvent(object):
     def __init__(self):
         self.time = datetime.datetime.utcnow()
 
 # Instantiate new event
-newEvent = Event()
+newEvent = DogEvent()
 print newEvent.time
+
+# TODO: Learn Flask-SQLAlchemy. http://flask-sqlalchemy.pocoo.org/2.1/quickstart/
+# Some sample functions:
+#   model.DogEvent.query.all() <- all events
+#   model.DogEvent.query.filter_by(visitor='james').all() <- all my visits
+#   model.DogEvent.query.filter_by(ate=1).all() <- all visits where the dog ate

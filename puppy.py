d30006c09a46aae9c85e6c2dae2d562f894e99c6
James Acklin <jamesacklin@gmail.com>
2016-04-27 00:04:42 -0400

    Consolidated behaviors, string outputs

--- puppy.py
+++ puppy.py
@@ -3,33 +3,20 @@ import datetime
 
 # Define event class
 class dogEvent(object):
-    def __init__(self, time, visitor, urinated, defocated, ate, bit, played):
+    def __init__(self, time, visitor, behaviors):
         self.time = time
         self.visitor = visitor
-        self.urinated = urinated
-        self.defocated = defocated
-        self.ate = ate
-        self.bit = bit
-        self.played = played
+        self.behaviors = behaviors
 
-# The primary logging function
-def logEvent():
-    # Instantiate new sample event
-    newEvent = dogEvent(datetime.datetime.now(), "James", 1, 1, 1, 0, 1)
+# Instantiate new sample event
+newEvent = dogEvent(datetime.datetime.now(), "James", ["peed", "pooped", "ate", "bit me", "played"])
 
-    # Pretty-print date and time
-    datestring = newEvent.time.strftime("%A, %B %d, %Y")
-    timestring = newEvent.time.strftime("%I:%M %p")
-
-    # Begin output
-    print newEvent.visitor + " visited the puppy on " + datestring + " at " + timestring
-
-# TODO: Wite a dictionary to say what the puppy did.
+# Begin output
+print "%s visited the puppy on %s at %s" % (newEvent.visitor, newEvent.time.strftime("%A, %B %d, %Y"), newEvent.time.strftime("%I:%M %p"))
+print "The puppy " + ', '.join([str(behavior) for behavior in newEvent.behaviors])
 
 # TODO: Learn Flask-SQLAlchemy. http://flask-sqlalchemy.pocoo.org/2.1/quickstart/
 # Some sample functions:
 #   model.DogEvent.query.all() <- all events
 #   model.DogEvent.query.filter_by(visitor='james').all() <- all my visits
 #   model.DogEvent.query.filter_by(ate=1).all() <- all visits where the dog ate
-
-logEvent()

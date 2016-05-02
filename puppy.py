a8fb89ccbd76d467f7a6caf1a41af691f9576b44
James Acklin <acklin@Jamess-MacBook-Air.local>
2016-05-02 17:06:49 -0400

    Notes from Hoopes

--- puppy.py
+++ puppy.py
@@ -31,6 +31,29 @@ print "The puppy " + ', '.join([str(behavior) for behavior in storedEvent.behavi
 # 7. Learn something about Flask and write a web front-end for this.
 # 8. Write a Flask-API and talk to the application that way.
 
+
+# Hoopes suggested to consider a DogEvent instance to be a single behavior
+# even if she does 3 things at once, make 3 events with the same timestamp
+#
+# id | time  | visitor | type
+# 1  | 12:00 | "James" | "pee"
+# 2  | 12:00 | "James" | "poop"
+# 3  | 12:05 | "Mary"  | "played"
+# 4  | 12:05 | "Mary"  | "ate
+#
+# This is for SQLAlchemy, but the point is making a set of events 
+# based on a for loop
+#
+# for b in ["pee", "poop", "ate"]:
+#    evt = DogEvent(visitor="James", type=b)
+#    puppy_app.session.add(evt)
+# puppy_app.session.commit()  # into the db they go
+#
+# Or even more clever...
+#
+# things = ['pee', 'poop', 'ate']
+# evts = [DogEvent(visitor='James', type=b) for b in things]
+
 # I've decided it's probably a stretch goal to learn SQLAlchemy and
 # Flask-SQLAlchemy. Whatever, life is a work.
 # http://flask-sqlalchemy.pocoo.org/2.1/quickstart/

d35e32cc4e8e78456bd331f579ad85a70e1f8ed6
James Acklin <acklin@Jamess-MacBook-Air.local>
2016-05-03 12:33:25 -0400

    Now writes to a SQLite DB

--- puppy.py
+++ puppy.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python
 import datetime
-import cPickle as pickle
 import argparse
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from table_def import Visit
 
-# Define event class
-class dogEvent(object):
-    def __init__(self, time, visitor, behavior):
-        self.time = time
-        self.visitor = visitor
-        self.behavior = behavior
+engine = create_engine('sqlite:///visits.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 # Creating a new event and logging it
 def logEvent(time, visitor, behavior):
-    newEvent = dogEvent(time, visitor, behavior)
-    pickle.dump(newEvent, open("log.txt", "wb"))
-    storedEvent = pickle.load(open("log.txt", "rb"))
-    print "The puppy %s when %s visited the puppy on %s at %s" % (storedEvent.behavior, storedEvent.visitor, storedEvent.time.strftime("%A, %B %d, %Y"), storedEvent.time.strftime("%I:%M %p"))
+    newEvent = Visit(time, visitor, behavior)
+    # print "The puppy %s when %s visited the puppy on %s at %s" % (newEvent.behavior, newEvent.visitor, newEvent.time.strftime("%A, %B %d, %Y"), newEvent.time.strftime("%I:%M %p"))
+    session.add(newEvent)
+    session.commit()
 
 # Reading from the command line
 def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--visitor", help="The name of the person who visited the dog.", required=True)
-    parser.add_argument("-b", "--behavior", help="Things the dog did.", nargs='+', required=True)
+    parser.add_argument("-b", "--behavior", help="Things the dog did, in the past tense.", nargs='+', required=True)
     args = parser.parse_args()
     if args.visitor and args.behavior:
         for behavior in args.behavior:
@@ -34,7 +33,7 @@ if __name__ == "__main__":
 # x 1. Write a new DogEvent() to a file based on some hard-coded values.
 # x 2. Read the last entry to the file back to me.
 # x 3. Write a new DogEvent() to a file with command-line arguments.
-# 4. Write a new DogEvent() to a database with command-line arguments.
+# x 4. Write a new DogEvent() to a database with command-line arguments.
 # 5. Read the last databse entry back to me.
 # 6. Tell me the last time the dog went on a walk using a database.
 # 7. Learn something about Flask and write a web front-end for this.
@@ -50,7 +49,7 @@ if __name__ == "__main__":
 # 3  | 12:05 | "Mary"  | "played"
 # 4  | 12:05 | "Mary"  | "ate
 #
-# This is for SQLAlchemy, but the point is making a set of events 
+# This is for SQLAlchemy, but the point is making a set of events
 # based on a for loop
 #
 # for b in ["pee", "poop", "ate"]:
---
+++ table_def.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# table_def.py
+
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+engine = create_engine('sqlite:///visits.db', echo=True)
+Base = declarative_base()
+
+class Visit(Base):
+    """"""
+    __tablename__ = "visits"
+    id = Column(Integer, primary_key=True)
+    time = Column(Date)
+    visitor = Column(String)
+    behavior = Column(String)
+    def __init__(self, time, visitor, behavior):
+        """"""
+        self.time = time
+        self.visitor = visitor
+        self.behavior = behavior
+
+Base.metadata.create_all(engine)
